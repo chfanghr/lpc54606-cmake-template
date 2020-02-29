e281bcf7e9c70452058263876ebf7204e550083d
fanghr <chfanghr@gmail.com>
2020-02-29 13:45:16 +0800

    Update

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,4 +1,4 @@
-FUNCTION(ELF2BIN target)
+FUNCTION(POST_BUILD_ELF2BIN target)
 	GET_FILENAME_COMPONENT(target_dir $<TARGET_FILE:${target}> DIRECTORY)
 	ADD_CUSTOM_COMMAND(
 			TARGET "${target}" POST_BUILD
@@ -17,9 +17,10 @@ FUNCTION(ADD_EXECUTABLE_CONV)
 			startup
 			cpp_config
 			board
-			power_hardabi)
-        TARGET_LINK_OPTIONS(${ARGV0} INTERFACE "-Xlinker -Map=${ARGV0}.map")
-	ELF2BIN(${ARGV0})
+			power_hardabi
+			arm_cortexM4lf_math)
+	TARGET_LINK_OPTIONS(${ARGV0} INTERFACE "-Xlinker -Map=${ARGV0}.map")
+	POST_BUILD_ELF2BIN(${ARGV0})
 ENDFUNCTION()
 
 
